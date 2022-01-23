7e77cd46ab340f57a95506eb8dee0f85ce4d0b03
iiivy <46492171+ivy-Ruxin-Tong@users.noreply.github.com>
2022-01-22 22:27:21 -0800

    Update SnowflakeConnection.py

--- snowflake/SnowflakeConnection.py
+++ snowflake/SnowflakeConnection.py
@@ -2,9 +2,9 @@ from snowflake import connector
 
 def sfconnect():
     cnx = connector.connect(
-    account = 'jp81237.us-central1.gcp',
-    user = 'ivytong',
-    password = 'Seattlenewlife!12',
+    account = ,
+    user = ,
+    password = ,
     warehouse = 'COMPUTE_WH',
     database = 'DEMO_DB',
     schema = 'Public'  
